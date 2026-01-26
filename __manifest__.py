07480fb4bdd3968bed573cee47554222871c85a2
Viktor Onodi <viktor.onodi.dev@gmail.com>
2026-01-26 15:10:00 +0000

    chore: initial addon skeleton

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Fulfillment Planner',
+    'version': '17.0.0.1.0',
+    'summary': 'Fulfillment planning control module',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
